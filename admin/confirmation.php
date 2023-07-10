7f380132fd46d74c7b02770f16af91fd4793f076
Lakshmishree <glakshmi2002@gmail.com>
2023-07-10 20:27:00 +0530

    Recruitment Management System

---
+++ admin/confirmation.php
@@ -0,0 +1,13 @@
+<?php 
+include 'db_connect.php';
+$application = $conn->query("SELECT  a.*,v.position FROM application a inner join vacancy v on v.id = a.position_id where a.id=".$_GET['id'])->fetch_array();
+foreach($application as $k => $v){
+	$$k = $v;
+}
+       $fname = explode('_',$resume_path);
+       unset($fname[0]);
+       $fname = implode("",$fname);
+?>
+<div class="container-fluid ">
+<p><b>Mail successfully sent to :</b><?php echo $email ?></p>
+</div>
